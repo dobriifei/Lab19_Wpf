25e6e8b59e10419132119d9e9f44375825cab6c7
vermei2@outlook.com <vermei2@outlook.com>
2022-03-01 21:38:05 +0300

    Добавьте файлы проекта.

---
+++ Lab19_Wpf/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19_Wpf.Models
+{
+   static class Ariph
+    {
+       public static int Add(int a, int b)=>a+b;
+    }
+}
